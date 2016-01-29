836cf3b7f3d782f8285acaf238db33acec74942e
danilolekovic <danilo@lekovic.ca>
2016-01-29 13:53:49 -0800

    gen: stash of variables

---
+++ source/gen/stash.d
@@ -0,0 +1,27 @@
+module iode.gen.stash;
+
+import std.stdio;
+import llvm.c;
+
+class Stash {
+    public static LLVMValueRef[string] namedValues;
+
+    public static LLVMValueRef getVariable(string name) {
+        if (checkVariable(name)) {
+            return namedValues[name];
+        } else {
+            throw new Exception("Variable " ~ name ~ " doesn't exist.");
+        }
+    }
+
+    public static void newVariable(string name, LLVMValueRef value) {
+        namedValues[name] = value;
+    }
+
+    public static bool checkVariable(string name) {
+        LLVMValueRef* p;
+        p = (name in namedValues);
+
+        return p !is null;
+    }
+}
