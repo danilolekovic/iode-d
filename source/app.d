d2171994bead328b513ede3d0257bdd75f232c25
danilolekovic <danilo@lekovic.ca>
2016-01-29 15:16:43 -0800

    test works!

--- source/app.d
+++ source/app.d
@@ -3,11 +3,23 @@ module main;
 import std.stdio;
 import iode.lexical.token;
 import iode.lexical.lexer;
+import iode.parsing.parser;
 import iode.ast.node;
 
 void main(string[] args) {
-	Lexer lexer = new Lexer("+");
+	Lexer lexer = new Lexer("var a = 2\n");
 	lexer.tokenize();
 
-	writeln(lexer.tokens[0].getType());
+	Parser parser = new Parser(lexer);
+	Node[] ast;
+
+	while (parser.pos != parser.totalTokens) {
+		ast ~= parser.start();
+	}
+
+	foreach (n; ast) {
+		writeln(n);
+	}
+
+	writeln("Done");
 }
