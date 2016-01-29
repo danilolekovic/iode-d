c9f680daf11a338deebc49dd31e7b0471b34a955
danilolekovic <danilo@lekovic.ca>
2016-01-29 00:32:02 -0800

    ast: NumberNode | codegen: NumberNode

--- source/app.d
+++ source/app.d
@@ -3,6 +3,7 @@ module main;
 import std.stdio;
 import iode.lexical.token;
 import iode.lexical.lexer;
+import iode.ast.node;
 
 void main(string[] args) {
 	Lexer lexer = new Lexer("+");
--- source/ast/node.d
+++ source/ast/node.d
@@ -3,4 +3,17 @@ module iode.ast.node;
 import llvm.c;
 
 interface Node {
+    LLVMValueRef generate();
+}
+
+class NodeNumber : Node {
+    private double value;
+
+    this(double value) {
+        this.value = value;
+    }
+
+    LLVMValueRef generate() {
+        return LLVMConstReal(LLVMDoubleType(), value);
+    }
 }
