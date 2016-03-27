31c76a6cc4c067c330c45473a1f7ee0da618a878
danilolekovic <danilo@lekovic.ca>
2016-03-26 19:35:25 -0700

    CLI finished

--- source/app.d
+++ source/app.d
@@ -9,19 +9,29 @@ import iode.gen.codeGen;
 void main(string[] args) {
 	string[] files;
 	bool help = false;
-	bool compile = false;
+	bool vrsn = false;
 
 	foreach (string arg; args) {
-		if (arg == "-c" || arg == "--compile") {
-			compile = true;
-		} else if (arg == "-h" || arg == "--help") {
+		if (arg == "-h" || arg == "--help") {
 			help = true;
+		} else if (arg == "-v" || arg == "--version") {
+			vrsn = true;
 		} else {
 			files ~= arg;
 		}
 	}
 
-	removeAt(files, 0);
+	if (help) {
+		getHelp();
+	}
+
+	if (vrsn) {
+		getVersion();
+	}
+
+	if (files.length > 0) {
+		removeAt(files, 0);
+	}
 
 	foreach (string filePath; files) {
 		if (exists(filePath) != 0) {
@@ -49,10 +59,33 @@ void main(string[] args) {
 	}
 }
 
-static void removeAt(T)(ref T[] arr, size_t index)
-{
-    foreach (i, ref item; arr[index .. $ - 1])
+void getHelp() {
+	writeln();
+	writeln("The Iode Programming Language");
+	writeln();
+	writeln("Usage: iode <options> [files]");
+	writeln();
+	writeln("Options:");
+	writeln("\t-v, --version         returns Iode version");
+	writeln("\t-h, --help            returns usage info");
+	writeln();
+	writeln("Examples:");
+	writeln("\tiode -v");
+	writeln("\tiode test.iode test2.iode tests/test3.iode");
+	writeln();
+}
+
+void getVersion() {
+	writeln();
+	writeln("Iode v1.0.0");
+	writeln();
+}
+
+static void removeAt(T)(ref T[] arr, size_t index) {
+    foreach (i, ref item; arr[index .. $ - 1]) {
         item = arr[i + 1];
+	}
+
     arr = arr[0 .. $ - 1];
     arr.assumeSafeAppend();
 }
